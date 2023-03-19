b5ce1cb3f6c6692a669fcee70ec0c902a8f69c4a
sumit <1908321@sbsstc.ac.in>
2023-03-19 11:18:39 +0530

    first commit

---
+++ prctice.js
@@ -0,0 +1,21 @@
+function task1(){
+    console.log("task one");
+}
+function task2(){
+    console.log("task two");
+}
+function task3(){
+    console.log("task three");
+}
+function task4(){
+    console.log("task four");
+}
+
+function task5(){
+    console.log("task five");
+}
+task1();
+task2();
+task3();
+task4();
+task5();
